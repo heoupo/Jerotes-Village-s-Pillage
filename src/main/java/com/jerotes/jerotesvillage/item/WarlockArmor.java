90305642f405893a291a3a67d536738cf72f35b7
heoupo <He434700828Po@163.com>
2026-04-04 15:17:20 +0800

    Changes

---
+++ src/main/java/com/jerotes/jerotesvillage/item/WarlockArmor.java
@@ -0,0 +1,115 @@
+package com.jerotes.jerotesvillage.item;
+
+import com.jerotes.jerotes.item.Interface.ItemModelArmor;
+import com.jerotes.jerotesvillage.client.model.Modelwarlock_tiara;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+
+public abstract class WarlockArmor extends ArmorItem implements ItemModelArmor {
+	public WarlockArmor(Type type, Properties properties) {
+		super(new ArmorMaterial() {
+			@Override
+			public int getDurabilityForType(Type type) {
+				return new int[]{13, 15, 16, 11}[type.getSlot().getIndex()] * 35;
+			}
+
+			@Override
+			public int getDefenseForType(Type type) {
+				return new int[]{2, 4, 4, 5}[type.getSlot().getIndex()];
+			}
+
+			@Override
+			public int getEnchantmentValue() {
+				return 45;
+			}
+
+			@Override
+			public SoundEvent getEquipSound() {
+				return SoundEvents.ARMOR_EQUIP_LEATHER;
+			}
+
+			@Override
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of(new ItemStack(Items.IRON_INGOT));
+			}
+
+			@Override
+			public String getName() {
+				return "warlock_armor";
+			}
+
+			@Override
+			public float getToughness() {
+				return 1f;
+			}
+
+			@Override
+			public float getKnockbackResistance() {
+				return 0f;
+			}
+		}, type, properties);
+	}
+
+	@Override
+	public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+		super.initializeClient(consumer);
+		consumer.accept(new IClientItemExtensions() {
+			public HumanoidModel<?> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, HumanoidModel<?> original) {
+				Modelwarlock_tiara model = new Modelwarlock_tiara(Minecraft.getInstance().getEntityModels().bakeLayer(
+						Modelwarlock_tiara.LAYER_LOCATION)).getArmor(livingEntity);
+				model.hat.visible = equipmentSlot == EquipmentSlot.HEAD;
+				model.body.visible = equipmentSlot == EquipmentSlot.CHEST;
+				model.rightArm.visible = equipmentSlot == EquipmentSlot.CHEST;
+				model.leftArm.visible = equipmentSlot == EquipmentSlot.CHEST;
+				model.rightLeg.visible = equipmentSlot == EquipmentSlot.FEET;
+				model.leftLeg.visible = equipmentSlot == EquipmentSlot.FEET;
+				model.young = original.young;
+				model.crouching = original.crouching;
+				model.riding = original.riding;
+				model.rightArmPose = original.rightArmPose;
+				model.leftArmPose = original.leftArmPose;
+				return model;
+			}
+		});
+	}
+
+	public static class Helmet extends WarlockArmor {
+		public Helmet() {
+			super(Type.HELMET, new Properties().fireResistant().rarity(Rarity.RARE));
+		}
+
+		@Override
+		public void appendHoverText(ItemStack itemStack, Level level, List<Component> list, TooltipFlag tooltipFlag) {
+			list.add(this.getDisplayName_0().withStyle(ChatFormatting.GRAY));
+			list.add(this.getDisplayName_1().withStyle(ChatFormatting.GRAY));
+		}
+
+		public MutableComponent getDisplayName_0() {
+			return Component.translatable(this.getDescriptionId() + ".desc_0");
+		}
+		public MutableComponent getDisplayName_1() {
+			return Component.translatable(this.getDescriptionId() + ".desc_1");
+		}
+
+		@Override
+		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
+			return "jerotesvillage:textures/models/armor/warlock_tiara_layer_1.png";
+		}
+	}
+}
